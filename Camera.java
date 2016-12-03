27922a5fcb78e0760d00c6dccc74e83d3130b3d1
Md Abedin <Md Abedin>
2016-12-03 11:38:11 -0500

    Made basic files

--- Camera.java
+++ Camera.java
@@ -1,6 +1,8 @@
-
 public class Camera{
     int[] position;
     int width = 0, length = 0;
-    
+
+    public Camera(){
+	
+    }
 }
---
+++ EngineElement.java
@@ -0,0 +1,9 @@
+abstract class EngineElement{
+    int[] position;
+    java.awt.image.BufferedImage image;
+    
+    public EngineElement(int[] position, java.awt.image.BufferedImage image){
+	this.position = position;
+	this.image = image;
+    }
+}
